30a80d7630a7e4a5b690064c1714f31dfbc12a59
Lucas Boulle <lucasboullefigueiredo@gmail.com>
2024-01-27 18:01:49 -0300

    adding planning poker page

---
+++ src/pages/PlanningPoker.tsx
@@ -0,0 +1,11 @@
+export const PlanningPoker = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </> 
+  )
+}
